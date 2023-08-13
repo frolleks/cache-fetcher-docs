0ea7d2b559ad5f0b8ab57befc3832adbe2d79d52
frolleks <frolleks@hotmail.com>
2023-08-13 10:17:57 +0700

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <b>cache-fetcher</b>,
+  project: {
+    link: "https://github.com/frolleks/cache-fetcher",
+  },
+  docsRepositoryBase: "https://github.com/frolleks/cache-fetcher-docs",
+  footer: {
+    text: "Copyright © 2023 Frolleks | MIT License",
+  },
+};
+
+export default config;
